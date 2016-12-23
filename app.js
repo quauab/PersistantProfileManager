d6f744c5049adc3e2797b95dfe5e74cefe5809b5
Rick Walker <ichglauben@gmail.com>
2016-12-23 11:02:19 -0500

    Created custom module that provides functionality to capitalizes the first letter in a string of one or more words

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var express = require('express'),
     uuid = require('uuid'),
     chalk = require('chalk'),
     filter = require('./modules/conret'),
+    cfc = require('./modules/cfc'),
 	app = express();
 
 const NodeCouchDb = require('node-couchdb');
@@ -22,51 +23,6 @@ const couch = new NodeCouchDb(authorization);
 couch.listDatabases().then(function(dbs){
     // console.log(dbs);
 });
-
-function capitalizeFirstCharacter(word) {
-	var word_split = null,
-		line = "";
-	if (word.trim().toLowerCase() === 'id' ||
-		word.trim().toLowerCase() === 'ssn' ||
-		word.trim().toLowerCase() === 'sku' ||
-		word.trim().toLowerCase() === 'vm' ||
-		word.trim().toLowerCase() === 'mac' ||
-		word.trim().toLowerCase() === 'imei' ||
-		word.trim().toLowerCase() === 'os' ||
-		word.trim().toLowerCase() === 'atm' ||
-		word.trim().toLowerCase() === 'pa') {
-		word = word.toUpperCase();
-	} else if (word.match(/[-]/)) {
-		if (null !== (word_split = word.split(['-'])).length > 0) {
-			for (var i = 0; i<word_split.length; i++) {
-				if (i < (word_split.length - 1)) {
-					line += word_split[i].substring(0,1).toUpperCase() + word_split[i].substring(1) + '-';
-				} else {
-					line += word_split[i].substring(0,1).toUpperCase() + word_split[i].substring(1);
-				}
-			}
-			return line;
-		}
-	} else if (word.match(/[ ]/)) {
-		if (null !== (word_split = word.split([' '])).length > 0) {
-			for (var i = 0; i<word_split.length; i++) {
-				if (i < (word_split.length - 1)) {
-					line += word_split[i].substring(0,1).toUpperCase() + word_split[i].substring(1) + ' ';
-				} else {
-					line += word_split[i].substring(0,1).toUpperCase() + word_split[i].substring(1);
-				}
-			}
-			return line;
-		}
-	} else {
-		return word.substring(0,1).toUpperCase() + word.substring(1);
-	}
-	return word;
-}
-
-function cfc(word) {
-	return capitalizeFirstCharacter(word);
-}
 	
 // View engine
 app.set('view engine', 'ejs');
